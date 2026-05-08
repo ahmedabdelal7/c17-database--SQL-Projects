5948c8eb1738ddcaeb0c0d31055324032be5d995
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 20:44:27 +0300

    Add (Problem-35)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -474,8 +474,10 @@ select * from VehicleDetails
 where Engine_CC = (select Min(Engine_CC)  from VehicleDetails )
 
 
+--Problem 35: Get all vehicles that have the Maximum Engine_CC
 
-
+select * from VehicleDetails
+where Engine_CC = (select Max(Engine_CC) from VehicleDetails)
 
 
 
