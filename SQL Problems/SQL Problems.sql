c7b79f36f53e2ced7e450cff8777a3ae6cfb85c9
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 21:02:32 +0300

    Add (Problem-40)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -504,9 +504,13 @@ order by Engine_CC DESC
 select distinct top 3 Engine_CC from VehicleDetails
 order by Engine_CC DESC
 
+--Problem 40: Get all vehicles that has one of the Max 3 Engine CC
 
-
-
+select * from VehicleDetails
+where Engine_CC in
+(
+	select distinct top 3 Engine_CC from VehicleDetails order by Engine_CC DESC
+)
 
 
 
