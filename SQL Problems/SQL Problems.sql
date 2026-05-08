8ee017ceaffc88d6e2a85f1637d9e5b6acfd3143
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 19:13:45 +0300

    Add (Problem-25)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -378,6 +378,109 @@ where SubModelName ='Elite'
 select * from VehicleDetails
 where Engine_Liter_Display > 3 and NumDoors = 2
 
+--Problem 25: Get make and vehicles that the engine contains 'OHV' and have Cylinders = 4
+
+select  Makes.Make, VehicleDetails.* 
+from VehicleDetails inner join Makes on VehicleDetails.MakeID = Makes.MakeID
+where (VehicleDetails.Engine Like N'%OHV%') and (VehicleDetails.Engine_Cylinders = 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
